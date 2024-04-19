b15e7651e473f0e92620753deac988ec8928c0a5
Mekan777-alt <mekan2678@gmail.com>
2024-04-19 19:27:02 +0300

    added models to schedule and add migrations

--- schedule/models.py
+++ schedule/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+
+class Schedule(models.Model):
+    day_of_week = models.CharField('День недели', max_length=150)
+    number = models.IntegerField()
+    time_from = models.TimeField()
+    time_to = models.TimeField()
+    name_of_discipline = models.CharField('Наименование дисциплины', max_length=150)
+    type_of_lesson = models.CharField('Вид занятий', max_length=150)
+    teacher = models.CharField('Преподаватель', max_length=150)
+    place_of_perfomance = models.CharField('Место проведение', max_length=150)
+
+    def __str__(self):
+
+        return f"{self.type_of_lesson} - {self.name_of_discipline} - {self.teacher}"
