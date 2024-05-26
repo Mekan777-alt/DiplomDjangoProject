55e4c1e7b64bcdb1db79b1ac0f424d0357d41c41
Mekan777-alt <mekan2678@gmail.com>
2024-05-26 20:49:27 +0300

    add new template

--- schedule/urls.py
+++ schedule/urls.py
@@ -5,4 +5,5 @@ app_name = 'schedule'
 
 urlpatterns = [
     path('schedule/', views.schedule_view, name='schedule'),
+    path('session_schedule/', views.session_schedule_view, name='session_schedule_view'),
 ]
--- schedule/views.py
+++ schedule/views.py
@@ -22,3 +22,25 @@ def schedule_view(request):
     }
 
     return render(request, 'schedule/schedule.html', context)
+
+
+@login_required
+def session_schedule_view(request):
+    schedule_data = {}
+    groups = Schedule.objects.filter(type_schedule__schedule_type='Сессия').values_list('group__name', flat=True).distinct()
+    days_of_week = ['monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday']
+    day_names = dict(Schedule.DAYS_OF_WEEK)
+
+    for group in groups:
+        group_schedule = Schedule.objects.filter(group__name=group, type_schedule__schedule_type='Сессия').order_by('day_of_week', 'number')
+        schedule_data[group] = {}
+        for day in days_of_week:
+            schedule_data[group][day] = group_schedule.filter(day_of_week=day)
+
+    context = {
+        'session_schedule': Schedule.objects.filter(type_schedule__schedule_type='Сессия'),
+        'schedule_data': schedule_data,
+        'day_names': day_names,
+    }
+
+    return render(request, 'schedule/session_schedule.html', context)
